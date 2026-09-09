25c5a6c6e0ea7689d40f8e18faba009f9bf69607
Zapikanka1111 <evakyl6@gmail.com>
2026-09-09 09:24:02 +0300

    Lab 1: виправлено значення x, y, z на коректні

--- lab1_Kylichenko.cpp
+++ lab1_Kylichenko.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    int x = 1, y = 2, z = 3;
+    double x = 0.48, y = 0.47, z = 1.32;
     double  b = ((y + atan(pow((fabs(pow(x, 2) + z)), 1/3)))/(2*z + x + pow(sin(pow(y, 3)), 2)))+exp(-((x + z)/(x +1)));
 
     double  a = (pow((1+y), 2))*(((pow((fabs(x+y)), 0.3))/pow(b, 2) + z)/(1+exp(-(x-y)) + pow(fabs(y), 0.43)));
