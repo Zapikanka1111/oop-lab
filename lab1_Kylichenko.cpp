01e84fa6d68582b510a2e94fdc3bd6ca33af8eaf
Zapikanka1111 <evakyl6@gmail.com>
2026-09-09 09:47:44 +0300

    Lab 1: перероблено під використання функції, щоб не вводити кожного разу формулу

--- lab1_Kylichenko.cpp
+++ lab1_Kylichenko.cpp
@@ -2,25 +2,29 @@
 #include <cmath>
 using namespace std;
 
+double calculateB(double x, double y, double z) {
+    return ((y + atan(pow((fabs(pow(x, 2) + z)), 1/3))) / (2*z + x + pow(sin(pow(y, 3)), 2))) + exp(-((x + z)/(x + 1)));
+}
+
+double calculateA(double x, double y, double z, double b) {
+    return (pow((1+y), 2)) * (((pow((fabs(x+y)), 0.3)) / pow(b, 2) + z) / (1 + exp(-(x-y)) + pow(fabs(y), 0.43)));
+}
+
 int main(){
     double x = 0.48, y = 0.47, z = 1.32;
-    double  b = ((y + atan(pow((fabs(pow(x, 2) + z)), 1/3)))/(2*z + x + pow(sin(pow(y, 3)), 2)))+exp(-((x + z)/(x +1)));
 
-    double  a = (pow((1+y), 2))*(((pow((fabs(x+y)), 0.3))/pow(b, 2) + z)/(1+exp(-(x-y)) + pow(fabs(y), 0.43)));
+    double b = calculateB(x, y, z);
+    double a = calculateA(x, y, z, b);
 
     cout << "task 1" "\nresult b: " << b << "\nresult a: " << a << endl;
-
-    a = 0;
-    b = 0;
 
     cout << "\nTask 2" << endl;
 
     for(double x = -1; x <= 1; x = x + 0.2){
-        b = ((y + atan(pow((fabs(pow(x, 2) + z)), 1/3)))/(2*z + x + pow(sin(pow(y, 3)), 2)))+exp(-((x + z)/(x +1)));
+        double b2 = calculateB(x, y, z);
+        double a2 = calculateA(x, y, z, b2);
 
-        a = (pow((1+y), 2))*(((pow((fabs(x+y)), 0.3))/pow(b, 2) + z)/(1+exp(-(x-y)) + pow(fabs(y), 0.43)));
-
-        cout << "\nx: " << x << "\nresult b: " << b << "\nresult a: " << a << endl;
+        cout << "\nx: " << x << "\nresult b: " << b2 << "\nresult a: " << a2 << endl;
     }
 
     return 0;
